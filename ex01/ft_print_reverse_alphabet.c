504083063c2afee23c58bb1e6a3900ffd3cf12d4
llndhlov <llndhlov@student.wethinkcode.co.za>
2020-07-09 17:47:37 +0200

    print reverse

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -6,16 +6,10 @@
 /*   By: llndhlov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 10:36:06 by llndhlov          #+#    #+#             */
-/*   Updated: 2020/07/09 17:07:16 by llndhlov         ###   ########.fr       */
+/*   Updated: 2020/07/09 17:46:52 by llndhlov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
-
 int		ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
 
 void	ft_print_reverse_alphabet(void)
 {
@@ -28,4 +22,4 @@ void	ft_print_reverse_alphabet(void)
 		i--;
 	}	
 	 	ft_putchar('\n');
-}		
+}	
