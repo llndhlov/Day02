ea42306a80b38dba6599d704bb962a51f2e05eed
llndhlov <llndhlov@student.wethinkcode.co.za>
2020-07-09 17:49:58 +0200

    negative02

--- ex03/ft_is_negative.c
+++ ex03/ft_is_negative.c
@@ -6,17 +6,11 @@
 /*   By: llndhlov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/09 11:11:58 by llndhlov          #+#    #+#             */
-/*   Updated: 2020/07/09 17:19:26 by llndhlov         ###   ########.fr       */
+/*   Updated: 2020/07/09 17:49:11 by llndhlov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
 int		ft_putchar(char c)
-{
-	write(1, &c, 1);
-	return (0);
-}
 
 void	ft_is_negative(int n)
 {
